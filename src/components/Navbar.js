f28b2c8f8676927f86cb1df33712867bace6fb4a
kevin <118621806+kevinKafrouni@users.noreply.github.com>
2023-12-16 12:45:14 +0200

    t

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import {Link} from 'react-router-dom'
 
 function Navbar(){
     return(
-
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link to="" className="flex items-center space-x-3 rtl:space-x-reverse">
