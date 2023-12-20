2fa9e36a6b6368e7748831fb7b049096a8d64714
kevin <118621806+kevinKafrouni@users.noreply.github.com>
2023-12-20 19:06:12 +0200

    usericon change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,9 @@
 import {Link} from 'react-router-dom'
 import { useState } from 'react';
+import UserIcon from './ui/UserIcon';
 function Navbar(){
 
-    const [profileVisible,setProfileVisible] = useState(false);
-
-    const toggleProfile = ()=>{
-        setProfileVisible(!profileVisible);
-    }
+    const [isLoggedin, setIsLoggedin] = useState(true);
     return(
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -14,25 +11,18 @@ function Navbar(){
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Icareer</span>
     </Link>
-    <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom" onClick={toggleProfile}>
-            <img className="w-10 h-10 rounded-full" src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        </button>
-        <div className={`absolute top-12 z-50 ${profileVisible?"":"hidden"} my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} id="user-dropdown">
-            <div className="px-4 py-3">
-            <span className="block text-sm text-gray-900 dark:text-white">Kevin</span>
-            <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@gmail.com</span>
-            </div>
-            <ul className="py-2" aria-labelledby="user-menu-button">
-            <li>
-                <Link to="" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
-            </li>
-            <li>
-                <Link to="" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
-            </li>
-            </ul>
-        </div>
+
+    {!isLoggedin? 
+    <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse font-medium">
+    <Link to="/auth/login" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</Link>
     </div>
+    :
+    <UserIcon 
+        name="kevin"
+        email="kevin@gmail.com"
+        image="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    />
+    }
     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
--- src/pages/authentification/RegisterForm.js
+++ src/pages/authentification/RegisterForm.js
@@ -1,23 +1,86 @@
 import { Link } from "react-router-dom";
+import { useState,useRef,useEffect } from "react";
+
+const USER_REGEX = /^[a-zA-Z ]+$/;
+const PASS_REGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 function RegisterForm(){
+
+  const userRef = useRef();
+  const errorRef = useRef();
+
+  const [firstName,setFirstName] = useState('');
+  const [validFname,setValidFname] = useState(false);
+
+  const [lastName,setLastName] = useState('');
+  const [validLname,setValidLname] = useState(false);
+
+  const [birthDate,setBirthDate] = useState('');
+  const [validBdate,setValidBdate] = useState(false);
+
+  const [email,setemail] = useState('');
+  const [validEmail,setValidEmail] = useState(false);
+
+  const [password,setPassword] = useState('');
+  const [validPassword,setValidPassword] = useState(false);
+
+  const [matchPass,setMatchPass] = useState('');
+  const [validMatchPass,setValidMatchPass] = useState(false);
+
+  const [errorMsg,setErrorMsg] = useState('');
+  const [success,setSuccess] = useState(false);
+
+  useEffect(()=>{
+    const result = USER_REGEX.test(firstName);
+    console.log(result);
+    console.log(firstName);
+    setValidFname(result);
+  },[firstName]);
+
+  useEffect(()=>{
+    const result = PASS_REGEX.test(password);
+    console.log(result);
+    console.log(password);
+    setValidPassword(result);
+    const match = password ===matchPass
+    setValidMatchPass(match)
+  },[password,matchPass]);
+
+  useEffect(()=>{
+    setErrorMsg('');
+  },[firstName,password,matchPass]);
     return(
         
         <div className="md:w-1/3 max-w-sm">
             <div className="text-center md:text-left">
                 <label className="mr-1">Create an account</label>
             </div>
-          <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Fist Name" />
+            <p ref={errorRef} className={success?"hidden":""} >{errorMsg}</p>
+            <form>
+          <input 
+                 id="firstname"
+                 ref={userRef}
+                 autoComplete="off"
+                 onChange={(e)=>setFirstName(e.target.value)}
+                 className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" 
+                 type="text" 
+                 placeholder="Fist Name"
+           />
           <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Last Name" />
           <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="date" placeholder="Birth Date"/>
           <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="text" placeholder="Email Address" />
           <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Password" />
+          <input className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4" type="password" placeholder="Verify Password" />
           <div className="mt-4 flex justify-between font-semibold text-sm">
             
           </div>
           <div className="text-center md:text-left">
-            <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider" type="submit">Register</button>
+            <button className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider" 
+                    type="submit"
+                    disabled={!validFname || !validPassword || !validEmail? true:false}
+                    >Register</button>
           </div>
+          </form>
           <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
             Already have an account? <Link to="/auth/login" className="text-red-600 hover:underline hover:underline-offset-4">Login</Link>
           </div>
