402c02254ea733dba22e3bc64017dfad3831d98d
khjennifer <khouryjennyfer@gmail.com>
2024-01-05 19:32:39 +0200

    update create profile

--- client/src/pages/userprofile/UserProfile.js
+++ client/src/pages/userprofile/UserProfile.js
@@ -1,6 +1,29 @@
 import ProfileInfo from './ProfileInfo';
 import ApplicationsTable from './ApplicationsTable';
+import { useNavigate } from "react-router-dom";
+
 function UserProfile(){
+    const navigate = useNavigate ();
+    useEffect(() => {
+        const checkLoginStatus = async () => {
+          try {
+            const res = await axios.get('http://localhost:8000/checklogin');
+            if (res.data.isLoggedIn) {
+              setIsLoggedin(true);
+              if(!res.data.type==="user"){ 
+                  navigate('LoginRegister');
+              }
+            }
+          } catch (error) {
+            console.error('Error checking login status:', error);
+          }
+        };
+        
+        checkLoginStatus();
+      }, []);
+
+
+
     return(
         <>
             <ProfileInfo />
